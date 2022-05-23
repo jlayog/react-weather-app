55b57b946cb0866a76222343e9408e6a854e0d32
jlayog <35618625+jlayog@users.noreply.github.com>
2022-05-23 16:03:02 -0400

    Hooks need fixing..
    useForecastFetch.js is a substitute hook until I figure out issues with Forecast.js

---
+++ src/components/Conditions.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Conditions = (props) => {
+    return (
+        <div>
+                <div>
+                    <p><strong>{props.data.location.name}</strong></p>
+                    <p>It is currently {Math.round(props.data.current.temp_f)} degrees out with {props.data.current.condition.text} skies.</p>
+                </div>           
+       </div>
+    )
+}
+
+export default Conditions;
---
+++ src/hooks/Forecast.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import Conditions from "../components/Conditions";
+
+
+
+const Forecast = () => {
+    const [data, setData] = useState({});
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value);
+    }
+
+
+    function fetchData() {
+        const options = {
+            method: 'GET',
+            headers: {
+                "CDN-Status": "200",
+            }
+        };
+        
+        fetch(`${process.env.REACT_APP_WEATHER_API_URL}/current.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=Baltimore&aqi=yes`, options)
+            .then(response => response.json())
+            .then(response => {
+                setData(response)
+            })
+            .catch(err => console.error(err));
+    }
+    
+    return (
+        <div>
+           <h2>Find Current Weather Conditions</h2>
+           <div>
+               {JSON.stringify(data)}
+           </div>
+           <form onSubmit={fetchData}>
+                <input type="text" placeholder="Enter city name" value={searchTerm} onChange={handleChange}/>
+                <button type="submit" value="Submit" />
+           </form>
+           <button onClick={fetchData}>Get Forecast</button>
+           <Conditions data={data} />
+       </div>
+    )
+}
+
+export default Forecast;
