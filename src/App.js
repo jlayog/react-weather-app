10f816d5ef4d03b1a48a7059bf21089ce9f0c753
jlayog <35618625+jlayog@users.noreply.github.com>
2022-05-26 23:51:12 -0400

    Added region. Setting up for case/switch icons
    icon quality of API is low, will import react-icons to serve as substitute

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>React Weather App</h1>
      </header>
      <main>
        <Forecast />
--- src/components/Conditions.js
+++ src/components/Conditions.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Conditions = (props) => {
+
     return (
         <div>
             {props.data.error ?
@@ -8,16 +9,15 @@ const Conditions = (props) => {
                     <p>{props.data.error.message}</p>
                 </div>
                 :
-                <div>
+                <div className="container">
+                    <h2><strong>{props.data.location.name}, {props.data.location.region}</strong></h2>
                     <div className="info-header">
-                        <h2>{Math.round(props.data.current.temp_f)}</h2>
+                        <h3>{Math.round(props.data.current.temp_f)}°F</h3>
                         <img className="forecast-icon" src={props.data.current.condition.icon} alt={props.data.current.condition.text} />
                     </div>
-                    <p><strong>{props.data.location.name}</strong></p>
-                    <p>It is currently {Math.round(props.data.current.temp_f)} degrees out with {props.data.current.condition.text.toLowerCase()} skies.</p>
+                    <p>It is currently {Math.round(props.data.current.temp_f)}°F degrees out with {props.data.current.condition.text.toLowerCase()} skies.</p>
                 </div>   
-            }
-                
+            }  
        </div>
     )
 }
