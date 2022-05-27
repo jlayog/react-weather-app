a4adc7f706625594a6fc64704e44bbaf9050e199
jlayog <35618625+jlayog@users.noreply.github.com>
2022-05-27 00:15:25 -0400

    Added last updated text

--- src/components/Conditions.js
+++ src/components/Conditions.js
@@ -16,6 +16,8 @@ const Conditions = (props) => {
                         <img className="forecast-icon" src={props.data.current.condition.icon} alt={props.data.current.condition.text} />
                     </div>
                     <p>It is currently {Math.round(props.data.current.temp_f)}°F degrees out with {props.data.current.condition.text.toLowerCase()} skies.</p>
+                    <p>It feels like {Math.round(props.data.current.feelslike_f)}°F.</p>
+                    <span className="last-update"><p>Last updated: {props.data.current.last_updated}</p></span>
                 </div>   
             }  
        </div>
